072195e4702b156948c2976ba28a687072173989
Justin Mackenzie <jmackenzie1908@gmail.com>
2021-07-22 20:55:33 -0400

    Add Median of Two Sorted Arrays

---
+++ InterviewQuestions.Solutions/MedianTwoSortedArrays.cs
@@ -0,0 +1,72 @@
+﻿/**
+ * Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+ * The overall run time complexity should be O(log (m+n)).
+ * 
+ * Input: nums1 = [1,3], nums2 = [2]
+ * Output: 2.00000
+ * Explanation: merged array = [1,2,3] and median is 2.
+ * 
+ * Input: nums1 = [1,2], nums2 = [3,4]
+ * Output: 2.50000
+ * Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+ */
+using System;
+
+namespace InterviewQuestions.Solutions
+{
+    public class MedianTwoSortedArrays
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            // We will refer to nums1 as array A and nums2 as array B.
+            int lengthA = nums1.Length;
+            int lengthB = nums2.Length;
+
+            if (lengthB > lengthA)
+            {
+                return FindMedianSortedArrays(nums2, nums1);
+            }
+
+            // Perform binary search on array A.
+            int start = 0;
+            int end = lengthA;
+
+            while (start <= end)
+            {
+                int partitionA = (end + start) / 2;
+                int partitionB = ((lengthA + lengthB + 1) / 2) - partitionA;
+
+                int maxLeftA = partitionA == 0 ? int.MinValue : nums1[partitionA - 1];
+                int minRightA = partitionA == lengthA ? int.MaxValue : nums1[partitionA];
+
+                int maxLeftB = partitionB == 0 ? int.MinValue : nums2[partitionB - 1];
+                int minRightB = partitionB == lengthB ? int.MaxValue : nums2[partitionB];
+
+                if (maxLeftA <= minRightB && maxLeftB <= minRightA)
+                {
+                    // We have found the correct partition.
+                    if ((lengthA + lengthB) % 2 == 0)
+                    {
+                        return (Math.Max(maxLeftA, maxLeftB) + Math.Min(minRightA, minRightB)) / 2.0;
+                    }
+                    else
+                    {
+                        return Math.Max(maxLeftA, maxLeftB);
+                    }
+                } 
+                else if (maxLeftA > minRightB)
+                {
+                    // The maximum value in A on the left side is too big, so move the partition to the left.
+                    end = partitionA - 1;
+                }
+                else
+                {
+                    // The minimum value in A on the right side is too small, so move the partition to the right.
+                    start = partitionA + 1;
+                }
+            }
+
+            throw new InvalidOperationException();
+        }
+    }
+}
---
+++ InterviewQuestions.Tests/MedianTwoSortedArraysTest.cs
@@ -0,0 +1,47 @@
+﻿using InterviewQuestions.Solutions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace InterviewQuestions.Tests
+{
+    public class MedianTwoSortedArraysTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            int[] a = new int[] { 1, 2, 3 };
+            int[] b = new int[] { 4, 5, 6 };
+
+            var solution = new MedianTwoSortedArrays();
+            var result = solution.FindMedianSortedArrays(a, b);
+
+            Assert.Equal(3.5, result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int[] a = new int[] { 1, 3, 8, 9, 15 };
+            int[] b = new int[] { 7, 11, 18, 19, 21, 25 };
+
+            var solution = new MedianTwoSortedArrays();
+            var result = solution.FindMedianSortedArrays(a, b);
+
+            Assert.Equal(11, result);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            int[] a = new int[] { 23, 26, 31, 35 };
+            int[] b = new int[] { 3, 5, 7, 9, 11, 16 };
+
+            var solution = new MedianTwoSortedArrays();
+            var result = solution.FindMedianSortedArrays(a, b);
+
+            Assert.Equal(13.5, result);
+        }
+    }
+}
